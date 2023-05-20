0625e641eec40b7a8ba4e1c740a0c24919f31089
Maurilio Felipe Rodrigues <maurilio.felipe@outlook.com>
2023-05-19 22:05:15 -0300

    Adicionado designer de Windows Form

--- ChessMovesViewer/Form.Designer.cs
+++ ChessMovesViewer/Form.Designer.cs
@@ -29,20 +29,87 @@ namespace ChessMovesViewer
         /// </summary>
         private void InitializeComponent()
         {
+            this.OptionsPanel = new System.Windows.Forms.Panel();
+            this.PiecesComboBox = new System.Windows.Forms.ComboBox();
+            this.GuidelinesLabel = new System.Windows.Forms.Label();
+            this.ContentPanel = new System.Windows.Forms.Panel();
+            this.OptionsPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // OptionsPanel
+            // 
+            this.OptionsPanel.Controls.Add(this.PiecesComboBox);
+            this.OptionsPanel.Controls.Add(this.GuidelinesLabel);
+            this.OptionsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OptionsPanel.Location = new System.Drawing.Point(20, 20);
+            this.OptionsPanel.Name = "OptionsPanel";
+            this.OptionsPanel.Size = new System.Drawing.Size(480, 40);
+            this.OptionsPanel.TabIndex = 0;
+            // 
+            // PiecesComboBox
+            // 
+            this.PiecesComboBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.PiecesComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.PiecesComboBox.FormattingEnabled = true;
+            this.PiecesComboBox.Items.AddRange(new object[] {
+            "Rei",
+            "Rainha",
+            "Torre",
+            "Bispo",
+            "Cavalo"});
+            this.PiecesComboBox.Location = new System.Drawing.Point(330, 0);
+            this.PiecesComboBox.Name = "PiecesComboBox";
+            this.PiecesComboBox.Size = new System.Drawing.Size(150, 27);
+            this.PiecesComboBox.TabIndex = 0;
+            // 
+            // GuidelinesLabel
+            // 
+            this.GuidelinesLabel.AutoSize = true;
+            this.GuidelinesLabel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.GuidelinesLabel.Location = new System.Drawing.Point(0, 0);
+            this.GuidelinesLabel.MinimumSize = new System.Drawing.Size(0, 27);
+            this.GuidelinesLabel.Name = "GuidelinesLabel";
+            this.GuidelinesLabel.Size = new System.Drawing.Size(316, 27);
+            this.GuidelinesLabel.TabIndex = 0;
+            this.GuidelinesLabel.Text = "Escolha uma peça e veja seus movimentos:";
+            this.GuidelinesLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ContentPanel
+            // 
+            this.ContentPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ContentPanel.Location = new System.Drawing.Point(20, 60);
+            this.ContentPanel.Name = "ContentPanel";
+            this.ContentPanel.Size = new System.Drawing.Size(480, 480);
+            this.ContentPanel.TabIndex = 1;
             // 
             // Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(520, 560);
+            this.Controls.Add(this.ContentPanel);
+            this.Controls.Add(this.OptionsPanel);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form";
-            this.Text = "Form";
+            this.Padding = new System.Windows.Forms.Padding(20);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chess Moves";
+            this.OptionsPanel.ResumeLayout(false);
+            this.OptionsPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel OptionsPanel;
+        private System.Windows.Forms.Panel ContentPanel;
+        private System.Windows.Forms.ComboBox PiecesComboBox;
+        private System.Windows.Forms.Label GuidelinesLabel;
     }
 }
 
